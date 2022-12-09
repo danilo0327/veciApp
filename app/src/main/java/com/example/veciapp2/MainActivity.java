4130392594b5409155b05ed3944ad8b3d5f1e307
danilo0327 <danilo.alfonsody@gmail.com>
2022-12-09 12:32:18 -0500

    feat: MainActivity added to the app project

--- app/src/main/java/com/example/veciapp2/MainActivity.java
+++ app/src/main/java/com/example/veciapp2/MainActivity.java
@@ -1,14 +1,80 @@
 package com.example.veciapp2;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.veciapp.Activity.Adaptor.CategoryAdaptor;
+import com.example.veciapp.Activity.Adaptor.PopularAdaptor;
+import com.example.veciapp.Activity.Domain.CategoryDomain;
+import com.example.veciapp.Activity.Domain.FoodDomain;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    private RecyclerView.Adapter adapter, adapter2;
+    private RecyclerView recyclerViewCategoryList, recyclerViewPopularList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        recyclerViewCategory();
+        recyclerViewPopular();
+        bottomNavigation();
+    }
+    private void bottomNavigation(){
+        FloatingActionButton floatingActionButton = findViewById(R.id.cartBtn);
+        LinearLayout homeBtn = findViewById(R.id.homeBtn);
+
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this,CartListActivity.class));
+            }
+        });
+        homeBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this,MainActivity.class));
+            }
+        });
+    }
+
+    private void recyclerViewCategory() {
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this,LinearLayoutManager.HORIZONTAL,false);
+        recyclerViewCategoryList = findViewById(R.id.recyclerView2);
+        recyclerViewCategoryList.setLayoutManager(linearLayoutManager);
+
+        ArrayList<CategoryDomain> category = new ArrayList<>();
+        category.add(new CategoryDomain("Supermercados","market"));
+        category.add(new CategoryDomain("Restaurantes","food"));
+        category.add(new CategoryDomain("Licores","liquor"));
+        category.add(new CategoryDomain("Droguerias","drugs"));
+        category.add(new CategoryDomain("Panaderias","bakery"));
+
+        adapter = new CategoryAdaptor(category);
+        recyclerViewCategoryList.setAdapter(adapter);
+    }
+    private void recyclerViewPopular() {
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this,LinearLayoutManager.HORIZONTAL,false);
+        recyclerViewPopularList = findViewById(R.id.recyclerview3);
+        recyclerViewPopularList.setLayoutManager(linearLayoutManager);
+
+        ArrayList<FoodDomain> foodList = new ArrayList<>();
+        foodList.add(new FoodDomain("Pizza Mexicana","pop_1","Rica pizza con jalapeños, carne, pico de gallo y maizitos.",10.500));
+        foodList.add(new FoodDomain("Hamburguesa Doble","pop_2","Hamburgesa doblecarne, tocineta, queso chedar y pepinillos",23.000));
+        foodList.add(new FoodDomain("Pizza de Pollo","pop_3","Rica pizza de pollo con champiñon y mucho queso",10.500));
+
+        adapter2 = new PopularAdaptor(foodList);
+        recyclerViewPopularList.setAdapter(adapter2);
+
     }
 }
