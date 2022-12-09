0804f920efc9348620116f78aa226427f082f764
danilo0327 <danilo.alfonsody@gmail.com>
2022-12-09 12:36:45 -0500

    feat: IntroActivity changed

--- app/src/main/java/com/example/veciapp2/IntroActivity.java
+++ app/src/main/java/com/example/veciapp2/IntroActivity.java
@@ -1,14 +1,29 @@
 package com.example.veciapp2;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.example.veciapp2.R;
 
 public class IntroActivity extends AppCompatActivity {
+    private ConstraintLayout startBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_intro);
+
+        startBtn = findViewById(R.id.startBtn);
+        startBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(IntroActivity.this,MainActivity.class));
+
+            }
+        });
     }
 }
